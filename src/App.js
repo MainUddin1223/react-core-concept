5571097c1aa70d6b4fc90b0f558b8c3dc4af8843
MainUddin1223 <2021md.main@gmail.com>
2022-03-19 20:48:41 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    (
+      <div className="App">
+        <Person name="hello" address='Dhaka'></Person>
+        <Person name="gello" address='Barisal'></Person>
+        <Person name="mello" address='Kumilla'></Person>
+        <Friend name="Suzan"></Friend>
+        <Friend name="Bipul"></Friend>
+        <Friend name="Shahin"></Friend>
+      </div>
+    )
   );
 }
 
+function Person(props) {
+  return <div className="person">
+    <h1>{props.name}</h1>
+    <p>{props.address}</p>
+  </div>
+}
+function Friend(props) {
+  return (
+    <div className="friend">
+      <h3>Name: {props.name}</h3>
+      <p>profession:Singer</p>
+    </div>
+  )
+}
 export default App;
